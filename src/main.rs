12c33c657c43d90cf8513797d872ab6f8186cd1e
Saurabh Srivastava <me@saurabhsrivastava.dev>
2022-10-17 20:03:05 +0530

    Adding a small module hierarchy

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+mod project_mgmt;
+
+use project_mgmt as pm;
+use project_mgmt::project;
+
 fn main() {
     println!("Hello, world!");
+    pm::init();
+    pm::new_project();
+    project::Project {};
 }
---
+++ src/project_mgmt/mod.rs
@@ -0,0 +1,11 @@
+pub mod project;
+
+// Module initialization
+pub fn init() {
+    println!("To be implemented..");
+}
+
+pub fn new_project() {
+    println!("To be implemented..");
+    project::Project {};
+}
